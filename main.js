50d6e72c9ee6e33cd7e6d4ad9000deabf421753e
yarin <yarin2852003@gmail.com>
2025-01-10 12:03:24 +0200

    Initial commit

--- main.js
+++ main.js
@@ -71,3 +71,27 @@ function updateDisplay(value) {
     const display = document.getElementById('display');
     display.textContent = value;
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const introVideo = document.getElementById("intro-video");
+    const loadingScreen = document.getElementById("loading-screen");
+
+    // מאזין לסיום הסרטון
+    introVideo.addEventListener("ended", () => {
+        if (loadingScreen) {
+            loadingScreen.style.opacity = 0;
+            setTimeout(() => {
+                loadingScreen.style.display = "none";
+                document.body.classList.remove("loading");
+                if (loadingAudio) {
+                    loadingAudio.pause(); // מפסיק את השמע
+                    loadingAudio.currentTime = 0; // מאפס את השמע
+                }
+            }, 500);
+        }
+    });
+});
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('loading-screen').classList.add('hidden');
+    }, 4500); // 4 שניות
+};
